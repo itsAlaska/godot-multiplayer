bcd8969130c6271cb09cee114e0dc8154a788c87
itsAlaska <smackey85@yahoo.com>
2025-03-01 09:11:47 -0500

    Adds functionality for a peer player to connect to a host players game

--- levels/menu/menu.gd
+++ levels/menu/menu.gd
@@ -1,16 +1,28 @@
 extends Control
 
 
+@export var ip_line_edit: LineEdit
+@export var status_label: Label
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	multiplayer.connection_failed.connect(_on_connection_failed)
+	multiplayer.connected_to_server.connect(_on_connected_to_server)
 
 # Called when the host presses the Host button
 func _on_host_button_pressed() -> void:
 	Lobby.create_game()
 
 func _on_join_button_pressed() -> void:
-	pass # Replace with function body.
+	Lobby.join_game(ip_line_edit.text)
+	status_label.text = "Connecting..."
 
 func _on_start_button_pressed() -> void:
 	pass # Replace with function body.
+
+func _on_connection_failed():
+	status_label.text = "Failed to connect..."
+
+func _on_connected_to_server():
+	status_label.text = "Connected!"
--- lobby.gd
+++ lobby.gd
@@ -42,17 +42,63 @@ func create_game():
 	# connected.
 	player_connected.emit(1, player_info)
 
+func join_game(address):
+	# This represents a player that is joining a hosted game.
+	var peer = ENetMultiplayerPeer.new()
+	# Like above, will return an error message if there was an error
+	var error = peer.create_client(address, PORT)
+	if error:
+		return error
+	# Store the player if everything was successful
+	multiplayer.multiplayer_peer = peer
+
 func _on_player_connected(id):
-	pass
+	# id is the id of the person that is the recipient of the data (the person
+	# that just connected) and then the connected person receives the info
+	# in the second argument
+	_register_player.rpc_id(id, player_info)
+	
+# any_peer means every player, not just the host, can run this function. 
+# Reliable means that it will continue to attempt sending the information
+# packets until it is confirmed to be sent through.
+# RPC = Remote Procedure Calls
+@rpc("any_peer", "reliable")
+func _register_player(new_player_info):
+	# This will store the ID of the player that sent this rpc. This function is
+	# being run on every other player's client, not the person who triggered
+	# the function.
+	var new_player_id = multiplayer.get_remote_sender_id()
+	# Updates this players data, ie. their info
+	players[new_player_id] = new_player_info
+	# This player signals to everyone their id and info 
+	player_connected.emit(new_player_id, new_player_info)
 
 func _on_player_disconnected(id):
-	pass
+	# Removes the player from the Players list
+	players.erase(id)
+	# Emits the signal to all other players to update that this player has left
+	player_disconnected.emit(id)
 	
 func _on_connected_to_server():
-	pass
+	# Every player gets a unique ID when they connect to the server
+	var peer_id = multiplayer.get_unique_id()
+	# Will take whatever is in the player_info variable and store it with the
+	# player's ID in the players dictionary
+	players[peer_id] = player_info
+	# Updates all players that this player has connected and shared their ID and
+	# info with them.
+	player_connected.emit(peer_id, player_info)
 
 func _on_connection_failed():
-	pass
+	# Simply sets the player object to null in case there was an issue 
+	# connecting. Keeps it clear for the following attempts.
+	multiplayer.multiplayer_peer = null
 	
 func _on_server_disconnected():
-	pass
+	# Similar to above, except it is when you are already connected to the 
+	# server and then disconnect.
+	multiplayer.multiplayer_peer = null
+	# Clears the players dictionary so that it will be blank for future connects
+	players.clear()
+	# Updates all the other players that this player has disconnected.
+	server_disconnected.emit()
